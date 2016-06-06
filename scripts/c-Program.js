d6f878dec2f0485c4c9908c4bee270e2d42030b4
Chris Harvey (work) <charvey@asce.org>
2016-06-06 15:21:04 -0400

    update program docs

--- scripts/c-Program.js
+++ scripts/c-Program.js
@@ -16,25 +16,32 @@ function resizeProgram() {
     $('.c-Program__Hn').css('width','')
     setProgramHeight($('.c-Program__Hn').length)
   } else {
-    $('.c-Program__Hn').width((100/$('.c-Program__Hn').length) + '%')
+    $('.c-Program__Hn').width(function () {
+    // $('.c-Program__Hn').css('flex-basis', function () {
+      return (100/($(this).siblings('.c-Program__Hn').length+1)) + '%'
+    })
     setProgramHeight(1)
   }
 }
 function updateProgram() {
-  $('.c-Program__Hn').removeClass('c-Program__Hn--selected')
-  $('.c-Program__Schedule').removeClass('c-Program__Schedule--hiddenBefore c-Program__Schedule--shown c-Program__Schedule--hiddenAfter')
-  $(this).parents('.c-Program__Hn')
-    .addClass('c-Program__Hn--selected')
-    .next('.c-Program__Schedule').addClass('c-Program__Schedule--shown')
-  $(this).parents('.c-Program__Hn').prevAll('.c-Program__Hn').next('.c-Program__Schedule').addClass('c-Program__Schedule--hiddenBefore')
-  $(this).parents('.c-Program__Hn').nextAll('.c-Program__Hn').next('.c-Program__Schedule').addClass('c-Program__Schedule--hiddenAfter')
+  $(this).parents('.c-Program').each(function () {
+    $(this).find('.c-Program__Hn').removeClass('c-Program__Hn--selected')
+    $(this).find('.c-Program__Schedule').removeClass('c-Program__Schedule--hiddenBefore c-Program__Schedule--shown c-Program__Schedule--hiddenAfter')
+  })
+  $(this).parents('.c-Program__Hn').each(function () {
+    $(this).addClass('c-Program__Hn--selected')
+      .next('.c-Program__Schedule').addClass('c-Program__Schedule--shown')
+    $(this).prevAll('.c-Program__Hn').next('.c-Program__Schedule').addClass('c-Program__Schedule--hiddenBefore')
+    $(this).nextAll('.c-Program__Hn').next('.c-Program__Schedule').addClass('c-Program__Schedule--hiddenAfter')
+  })
   resizeProgram()
 }
 $(window).resize(resizeProgram)
 $('.c-Program__Hn')
-  .find('label').addClass('o-Block-sK')
-  .find('time' ).addClass('o-Block-sK')
-  .find('span' ).addClass('o-Block-sK')
+  .find('label'   ).addClass('o-Block-sK')
+  .find('time'    ).addClass('o-Block-sK')
+  .find('span.js-').addClass('o-Block-sK')
+$('.c-Program__Hn').addClass('-z-1') // over .c-Program__Schedule
 $('.c-Program__Schedule').addClass('-xo-1') // fallback `order: 1;`
   .css('position','absolute').css('left', 0)
 $('.c-Program__Check:checked').each(updateProgram)

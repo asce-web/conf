25ef0e921a16d5f02754ed3f656e3e1a4b38cf55
Chris Harvey (work) <charvey@asce.org>
2016-06-28 12:41:48 -0400

    clean up styles and docs

---
+++ scripts/c-SponsorCarousel.js
@@ -0,0 +1,11 @@
+$('.c-SponsorCarousel').slick({
+  slidesToShow: 3
+, slidesToScroll: 1
+, mobileFirst: true
+, arrows: true
+, dots: true
+, autoplay: true
+, autoplaySpeed: 1000
+, draggable: true
+, swipeToSlide: true
+})

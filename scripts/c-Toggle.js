e345c71b590f84969cadd8350bb8345ba5405016
Chris Harvey <chrisharvey2pi@gmail.com>
2016-07-03 01:39:47 -0400

    move Toggle--on functionality to Drawer--open
    	deleted:    scripts/c-Toggle.js

--- scripts/c-Toggle.js
+++
@@ -1,9 +0,0 @@
-function updateToggle() {
-  if ($(this).prop('checked')) {
-    $(this).parents('.c-Toggle').addClass('c-Toggle--on')
-  } else {
-    $(this).parents('.c-Toggle').removeClass('c-Toggle--on')
-  }
-}
-$('.c-Toggle__Check:checked').each(updateToggle)
-$('.c-Toggle__Check').change(updateToggle)

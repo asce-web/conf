c9e691e1ca0097eea9c0f488b1cbaec19cc4a65b
Chris Harvey <chrisharvey2pi@gmail.com>
2016-10-13 10:46:27 -0400

    move adding pages to init(); remove _was_initialized

--- models/classes/ConfSite.class.js
+++ models/classes/ConfSite.class.js
@@ -29,7 +29,6 @@ module.exports = (function () {
     self._sponsor_levels   = []
     self._org_levels       = []
     self._supporters       = []
-    self._was_initialized = false
     self._conf_curr_key   = null
     self._conf_prev_key   = null
     self._conf_next_key   = null
@@ -132,37 +131,6 @@ module.exports = (function () {
   ConfSite.prototype.currentConference = function currentConference(conf_label) {
     if (arguments.length) {
       this._conf_curr_key = conf_label
-      ;(function (self) {
-        if (self._was_initialized) {
-          self.find('#main-menu')
-            .removeAll() //- NOTE IMPORTANT
-            .add(new ConfPage('Home', 'home.html')
-              .title(self.currentConference().name())
-              .description(self.currentConference().theme())
-              .setIcon('home')
-            )
-            .add(new ConfPage('Registration', 'registration.html')
-              .title(ConfPage.pageTitle(self))
-              .description('Register for ' + self.currentConference().name() + ' here.')
-              .setIcon('shopping_cart')
-            )
-            .add(new ConfPage('Program', 'program.html')
-              .title(ConfPage.pageTitle(self))
-              .description('Program and agenda of ' + self.currentConference().name() + '.')
-              .setIcon('event')
-            )
-            .add(new ConfPage('Hotel & Travel', 'location.html')
-              .title(ConfPage.pageTitle(self))
-              .description('Location and where to stay for ' + self.currentConference().name() + '.')
-              .setIcon('flight')
-            )
-            .add(new ConfPage('Speakers', 'speakers.html')
-              .title(ConfPage.pageTitle(self))
-              .description('Current and prospective speakers at ' + self.currentConference().name() + '.')
-              .setIcon('account_box')
-            )
-        }
-      })(this)
       return this
     } else {
       return this.getConference(this._conf_curr_key)
@@ -285,46 +253,70 @@ module.exports = (function () {
 
   // METHODS
   /**
-   * Initialize this site: add the proper top-level pages.
+   * Initialize this site: add the proper pages.
+   * This method may not be called until a current conference has been added.
+   * This method should only be called once per current conference.
    * Return this initialized site, if it had not been initialized before,
-   * else return `undefined`.
+   * else throw an error.
+   * @see ConfSite#currentConference()
    * @return {ConfSite=} this site || undefined
    */
   ConfSite.prototype.init = function init() {
     var self = this
-    if (!self._was_initialized) {
-      self._was_initialized = true
+    if (self.currentConference()) {
       return self
-        .add(new Page({ name: 'Top', url: '#top-menu' })
-          .add(new ConfPage(self.name(), 'index.html')
-            .title(self.name())
-            .description(self.tagline())
-            .setIcon('')
-          )
-          .add(new ConfPage('About', 'about.html')
-            .title(ConfPage.pageTitle(self))
-            .description('About ' + self.name() + '.')
-            .setIcon('info_outline')
-          )
-          .add(new ConfPage('Sponsor', 'sponsor.html')
-            .title(ConfPage.pageTitle(self))
-            .description('Sponsors of ' + self.name() + '.')
-            .setIcon('people')
-          )
-          .add(new ConfPage('Exhibit', 'exhibit.html')
-            .title(ConfPage.pageTitle(self))
-            .description('Exhibitors at ' + self.name() + '.')
-            .setIcon('work')
-          )
-          .add(new ConfPage('Contact', 'contact.html')
-            .title(ConfPage.pageTitle(self))
-            .description('Contact us for questions and comments about ' + self.name() + '.')
-            .setIcon('email')
-          )
+        .removeAll() //- NOTE IMPORTANT
+        .add(new ConfPage(self.name(), 'index.html')
+          .title(self.name())
+          .description(self.tagline())
+          .setIcon('')
         )
-        .add(new Page({ name: 'Main', url: '#main-menu' }))
-        .colors(Color.fromString('#660000'), Color.fromString('#ff6600')) // default Hokie colors
-    } else return
+        .add(new ConfPage('Home', 'home.html')
+          .title(self.currentConference().name())
+          .description(self.currentConference().theme())
+          .setIcon('home')
+        )
+        .add(new ConfPage('Registration', 'registration.html')
+          .title(ConfPage.pageTitle(self))
+          .description('Register for ' + self.currentConference().name() + ' here.')
+          .setIcon('shopping_cart')
+        )
+        .add(new ConfPage('Program', 'program.html')
+          .title(ConfPage.pageTitle(self))
+          .description('Program and agenda of ' + self.currentConference().name() + '.')
+          .setIcon('event')
+        )
+        .add(new ConfPage('Hotel & Travel', 'location.html')
+          .title(ConfPage.pageTitle(self))
+          .description('Location and where to stay for ' + self.currentConference().name() + '.')
+          .setIcon('flight')
+        )
+        .add(new ConfPage('Speakers', 'speakers.html')
+          .title(ConfPage.pageTitle(self))
+          .description('Current and prospective speakers at ' + self.currentConference().name() + '.')
+          .setIcon('account_box')
+        )
+        .add(new ConfPage('Sponsor', 'sponsor.html')
+          .title(ConfPage.pageTitle(self))
+          .description('Sponsors of ' + self.name() + '.')
+          .setIcon('people')
+        )
+        .add(new ConfPage('Exhibit', 'exhibit.html')
+          .title(ConfPage.pageTitle(self))
+          .description('Exhibitors at ' + self.name() + '.')
+          .setIcon('work')
+        )
+        .add(new ConfPage('About', 'about.html')
+          .title(ConfPage.pageTitle(self))
+          .description('About ' + self.name() + '.')
+          .setIcon('info_outline')
+        )
+        .add(new ConfPage('Contact', 'contact.html')
+          .title(ConfPage.pageTitle(self))
+          .description('Contact us for questions and comments about ' + self.name() + '.')
+          .setIcon('email')
+        )
+    } else return // throw an error stating you need to add a current conference first
   }
 
   // STATIC MEMBERS

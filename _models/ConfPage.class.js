e1b14579430314124b0e5ca9c278b2caadfa5861
Chris Harvey (work) <charvey@asce.org>
2017-02-02 14:56:19 -0500

    add hidden functionality to ConfPage

--- _models/ConfPage.class.js
+++ _models/ConfPage.class.js
@@ -19,6 +19,7 @@ module.exports = (function () {
     var self = this
     Page.call(self, { name: name, url: url })
     self._icon     = null
+    self._is_hidden = false
   }
   ConfPage.prototype = Object.create(Page.prototype)
   ConfPage.prototype.constructor = ConfPage
@@ -39,6 +40,23 @@ module.exports = (function () {
    */
   ConfPage.prototype.getIcon = function getIcon(fallback) {
     return (this._icon) ? Util.iconToString(this._icon, fallback) : ''
+  }
+
+  /**
+   * Hide or show this page.
+   * @param  {boolean=true} bool hides or shows this page
+   * @return {Page} this page
+   */
+  Page.prototype.hide = function hide(bool) {
+    this._is_hidden = (arguments.length) ? bool : true
+    return this
+  }
+  /**
+   * Get the hidden status of this page.
+   * @return {boolean} true if this page is hidden; false otherwise
+   */
+  Page.prototype.isHidden = function isHidden() {
+    return this._is_hidden
   }
 
   // STATIC MEMBERS

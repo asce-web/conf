17d43278350e3c14c4d8083bb48c3bf319d383dc
Chris Harvey (work) <charvey@asce.org>
2017-02-08 15:39:51 -0500

    update styles & scripts

--- scripts/o-Tabs.js
+++ scripts/o-Tabs.js
@@ -1,40 +1,34 @@
-function resizeProgram() {
-  function setProgramHeight(n) {
-    n = n || 1 // number of headings stacked vertically
-    $('.o-Tabs').height(function () {
-      var current_panel = $(this).find('.o-Tabs__Panel--shown')
-      var current_tab = current_panel.prev('.o-Tabs__Tab')
-      function totalHeight($query) {
-        return $query.height()
-          + parseInt($query.css('padding-top'))
-          + parseInt($query.css('margin-bottom'))
-      }
-      return totalHeight(current_panel) + (n * totalHeight(current_tab))
-    })
-  }
-  $('.o-Tabs__Panel--js').css('bottom', function () {
-    return parseInt($(this).parents('.o-Tabs').css('padding-bottom'))
+function resizeTabBlock() {
+  /*
+   * number of headings stacked vertically
+   * 480 == @media screen and (min-width: 30em)
+   */
+  var n = ($(window).width() < 480) ? $('.o-Tabs__Tab').length : 1
+  $('.o-Tabs').height(function () {
+    var current_panel = $(this).find('.o-Tabs__Panel--shown')
+    var current_tab = current_panel.prev('.o-Tabs__Tab')
+    function totalHeight($jq) {
+      return $jq.height()
+        + parseInt($jq.css('padding-top'))
+        + parseInt($jq.css('margin-bottom'))
+    }
+    return totalHeight(current_panel) + (n * totalHeight(current_tab))
   })
-  if ($(window).width() < 480) { // @media screen and (min-width: 30em)
-    setProgramHeight($('.o-Tabs__Tab').length)
-  } else {
-    setProgramHeight(1)
-  }
 }
-function updateProgram() {
+function updateTabs() {
   $(this).parents('.o-Tabs').each(function () {
     $(this).find('.o-Tabs__Tab').removeClass('o-Tabs__Tab--selected')
-    $(this).find('.o-Tabs__Panel').removeClass('o-Tabs__Panel--hiddenBefore o-Tabs__Panel--shown o-Tabs__Panel--hiddenAfter')
+    $(this).find('.o-Tabs__Panel').removeClass('o-Tabs__Panel--shown o-Tabs__Panel--hiddenBefore o-Tabs__Panel--hiddenAfter').removeAttr('aria-hidden')
   })
   $(this).parents('.o-Tabs__Tab').each(function () {
     $(this).addClass('o-Tabs__Tab--selected')
-      .next('.o-Tabs__Panel').addClass('o-Tabs__Panel--shown')
-    $(this).prevAll('.o-Tabs__Tab').next('.o-Tabs__Panel').addClass('o-Tabs__Panel--hiddenBefore')
-    $(this).nextAll('.o-Tabs__Tab').next('.o-Tabs__Panel').addClass('o-Tabs__Panel--hiddenAfter')
+      .next('.o-Tabs__Panel--js').addClass('o-Tabs__Panel--shown')
+    $(this).prevAll('.o-Tabs__Tab').next('.o-Tabs__Panel--js').addClass('o-Tabs__Panel--hiddenBefore').attr('aria-hidden', true)
+    $(this).nextAll('.o-Tabs__Tab').next('.o-Tabs__Panel--js').addClass('o-Tabs__Panel--hiddenAfter' ).attr('aria-hidden', true)
   })
-  resizeProgram()
+  resizeTabBlock()
 }
-$(window).resize(resizeProgram)
 $('.o-Tabs .o-Tabs__Panel').addClass('o-Tabs__Panel--js')
-$('.o-Tabs .o-Tabs__Check:checked').each(updateProgram)
-$('.o-Tabs .o-Tabs__Check').change(updateProgram)
+$('.o-Tabs .o-Tabs__Check:checked').each(updateTabs)
+$('.o-Tabs .o-Tabs__Check').change(updateTabs)
+$(window).resize(resizeTabBlock)

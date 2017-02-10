828628672db2acd70a378ccae8781e9c231a8646
Chris Harvey (work) <charvey@asce.org>
2017-02-10 14:47:09 -0500

    tabindex program links

--- scripts/o-Tabs.js
+++ scripts/o-Tabs.js
@@ -19,12 +19,13 @@ function updateTabs() {
   $(this).parents('.o-Tabs').each(function () {
     $(this).find('.o-Tabs__Tab').removeClass('o-Tabs__Tab--selected')
     $(this).find('.o-Tabs__Panel').removeClass('o-Tabs__Panel--shown o-Tabs__Panel--hiddenBefore o-Tabs__Panel--hiddenAfter').removeAttr('aria-hidden')
+      .find('a').removeAttr('tabindex')
   })
   $(this).parents('.o-Tabs__Tab').each(function () {
     $(this).addClass('o-Tabs__Tab--selected')
       .next('.o-Tabs__Panel--js').addClass('o-Tabs__Panel--shown')
-    $(this).prevAll('.o-Tabs__Tab').next('.o-Tabs__Panel--js').addClass('o-Tabs__Panel--hiddenBefore').attr('aria-hidden', true)
-    $(this).nextAll('.o-Tabs__Tab').next('.o-Tabs__Panel--js').addClass('o-Tabs__Panel--hiddenAfter' ).attr('aria-hidden', true)
+    $(this).prevAll('.o-Tabs__Tab').next('.o-Tabs__Panel--js').addClass('o-Tabs__Panel--hiddenBefore').attr('aria-hidden', true).find('a').attr('tabindex', -1)
+    $(this).nextAll('.o-Tabs__Tab').next('.o-Tabs__Panel--js').addClass('o-Tabs__Panel--hiddenAfter ').attr('aria-hidden', true).find('a').attr('tabindex', -1)
   })
   resizeTabBlock()
 }
